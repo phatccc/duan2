a9dc0927e03f76ee37b71a0206d10c0d640b1a0d
phatccc <levargaming@gmail.com>
2024-11-27 15:12:48 +0700

    first commit

--- src/slot2/renderHTML/app.jsx
+++ src/slot2/renderHTML/app.jsx
@@ -6,6 +6,8 @@ const App = () => {
                     <input type="text" />
                     <label htmlFor="name">Name</label>
                     <p>Hello</p>
+                    <input type="checkbox" />
+                    <label htmlFor="checkbox">Checkbox</label>
                 </code>
             </button>
         </div>
