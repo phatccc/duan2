17ae5d96d2d84d196ae41360bd321157a93fe16f
phatccc <levargaming@gmail.com>
2024-11-27 15:09:32 +0700

    first commit

---
+++ src/slot2/jsx-content/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <p>Hello , <strong>JSX</strong></p>
+  )
+}
+
+export default App;
---
+++ src/slot2/renderHTML/app.jsx
@@ -0,0 +1,15 @@
+const App = () => {
+    return (
+        <div>
+            <button>
+                <code>
+                    <input type="text" />
+                    <label htmlFor="name">Name</label>
+                    <p>Hello</p>
+                </code>
+            </button>
+        </div>
+    )
+}
+
+export default App;
